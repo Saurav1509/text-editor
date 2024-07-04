03b48b57c103aa85b9470c9a27f5ecd2b6e049ef
Saurav1509 <saurav.sayana@gmail.com>
2024-07-05 01:28:20 +0530

    clean code

--- src/editor.rs
+++ src/editor.rs
@@ -1,6 +1,7 @@
 use crossterm::event::{read, Event, Event::Key, KeyCode::Char, KeyEvent, KeyModifiers};
 mod terminal;
-use terminal::Terminal;
+use std::io::Error;
+use terminal::{Position, Size, Terminal};
 
 pub struct Editor {
     should_quit: bool,
@@ -18,9 +19,10 @@ impl Editor {
         result.unwrap();
     }
 
-    fn repl(&mut self) -> Result<(), std::io::Error> {
+    fn repl(&mut self) -> Result<(), Error> {
         loop {
             self.refresh_screen()?;
+
             if self.should_quit {
                 break;
             }
@@ -44,23 +46,27 @@ impl Editor {
         }
     }
 
-    fn refresh_screen(&self) -> Result<(), std::io::Error> {
+    fn refresh_screen(&self) -> Result<(), Error> {
+        Terminal::hide_cursor()?;
         if self.should_quit {
             Terminal::clear_screen()?;
-            print!("GoodBye. \r\n");
+            Terminal::crossterm_print("GoodBye. \r\n")?;
         } else {
             Self::draw_rows()?;
-            Terminal::move_cursor_to(0, 0)?;
+            Terminal::move_cursor_to(Position { x: 0, y: 0 })?;
         }
+        Terminal::show_cursor()?;
+        Terminal::execute()?;
         Ok(())
     }
 
-    fn draw_rows() -> Result<(), std::io::Error> {
-        let height = Terminal::size()?.1;
-        for current_row in 0..height {
-            print!("~");
-            if current_row + 1 < height {
-                print!("\r\n");
+    fn draw_rows() -> Result<(), Error> {
+        let Size { h, .. } = Terminal::size()?;
+        for current_row in 0..h {
+            Terminal::clear_line()?;
+            Terminal::crossterm_print("~")?;
+            if current_row + 1 < h {
+                Terminal::crossterm_print("\r\n")?;
             }
         }
         Ok(())
--- src/editor/terminal.rs
+++ src/editor/terminal.rs
@@ -1,33 +1,77 @@
 use crossterm::cursor::MoveTo;
-use crossterm::execute;
+use crossterm::cursor::{Hide, Show};
+use crossterm::style::Print;
+// use crossterm::execute;
+use crossterm::queue;
 use crossterm::terminal::{disable_raw_mode, enable_raw_mode, size, Clear, ClearType};
-use std::io::stdout;
+use std::io::Error;
+use std::io::{stdout, Write};
 
-pub struct Terminal {}
+#[derive(Copy, Clone)]
+pub struct Size {
+    pub w: u16,
+    pub h: u16,
+}
+
+#[derive(Copy, Clone)]
+pub struct Position {
+    pub x: u16,
+    pub y: u16,
+}
+
+pub struct Terminal;
 
 impl Terminal {
-    pub fn initialize() -> Result<(), std::io::Error> {
+    pub fn initialize() -> Result<(), Error> {
         enable_raw_mode()?; // What '?' does here. It unwraps the Result of enable_raw_mode for us.
                             //If it's an error, it returns the error immediately. If not, it continues.
         Self::clear_screen()?;
-        Self::move_cursor_to(0, 0)?;
+        Self::move_cursor_to(Position { x: 0, y: 0 })?;
+        Self::execute()?;
         Ok(())
     }
-    pub fn terminate() -> Result<(), std::io::Error> {
+    pub fn terminate() -> Result<(), Error> {
         disable_raw_mode()?;
         Ok(())
     }
-    pub fn clear_screen() -> Result<(), std::io::Error> {
-        execute!(stdout(), Clear(ClearType::All))?;
+    pub fn clear_screen() -> Result<(), Error> {
+        queue!(stdout(), Clear(ClearType::All))?;
         Ok(())
     }
 
-    pub fn move_cursor_to(col: u16, row: u16) -> Result<(), std::io::Error> {
-        execute!(stdout(), MoveTo(col, row))?;
+    pub fn clear_line() -> Result<(), Error> {
+        queue!(stdout(), Clear(ClearType::CurrentLine))?;
         Ok(())
     }
 
-    pub fn size() -> Result<(u16, u16), std::io::Error> {
-        size()
+    pub fn move_cursor_to(pos: Position) -> Result<(), Error> {
+        queue!(stdout(), MoveTo(pos.x, pos.y))?;
+        Ok(())
+    }
+
+    pub fn size() -> Result<Size, Error> {
+        let (w, h) = size()?;
+        Ok(Size { w, h })
+    }
+
+    pub fn hide_cursor() -> Result<(), Error> {
+        queue!(stdout(), Hide)?;
+        Ok(())
+    }
+
+    pub fn show_cursor() -> Result<(), Error> {
+        queue!(stdout(), Show)?;
+        Ok(())
+    }
+
+    pub fn crossterm_print(s: &str) -> Result<(), Error> {
+        queue!(stdout(), Print(s))?;
+        Self::execute()?;
+        Ok(())
+    }
+
+    pub fn execute() -> Result<(), Error> {
+        stdout().flush()?;
+        Ok(())
     }
 }
